312114f4c7d92b93c8d4b776fd59405368e67b56
chenweizhen <chenwz@asiainfo.com>
2022-11-21 22:22:35 +0800

    init: swagger2配置

---
+++ src/main/java/com/chenwz/mysite/config/Knife4jConfiguration.java
@@ -0,0 +1,43 @@
+package com.chenwz.mysite.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableWebMvc
+@Import(BeanValidatorPluginsConfiguration.class)
+@EnableSwagger2
+@EnableKnife4j
+public class Knife4jConfiguration {
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        //.title("swagger-bootstrap-ui-demo RESTful APIs")
+                        .description("# swagger-bootstrap-ui-demo RESTful APIs")
+                        .termsOfServiceUrl("http://www.xx.com/")
+                        .version("1.0")
+                        .build())
+                //分组名称
+                .groupName("1.X版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.chenwz.mysite.controller"))
+                .paths(PathSelectors.any())
+                .build()
+//                .ignoredParameterTypes(UserTokenDTO.class)  //忽略登录DTO
+                ;
+        return docket;
+    }
+}
--- src/main/java/com/chenwz/mysite/controller/HomeController.java
+++ src/main/java/com/chenwz/mysite/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.chenwz.mysite.controller;
 
 import com.chenwz.mysite.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,14 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/home")
+@Api(value = "首页接口",tags = "首页接口")
 public class HomeController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/test")
+    @ApiOperation("用户列表")
     public String test(){
         userService.getList();
         Map map = new HashMap<>();
